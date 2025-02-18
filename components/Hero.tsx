0a1e3906656a2cc5a521ad676b2e70480c3b6f49
Achraf Bayi <achrafbayi@icloud.com>
2025-02-18 13:31:42 -0500

    Fix syntax errors and update Tailwind classes in Hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
     <div className="pb-20">
         <div> 
             <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white"/>
-            <Spotlight className="top-10 left-full h-[80vh] w-[50vm]" fill="purple"/>
+            <Spotlight className="top-10 left-full h-[80vh] w-[50vh]" fill="purple"/>
             <Spotlight className="top-28 left-80 h-[80vh] w-[50vh]" fill="blue"/>
             <div>
             <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0"/>
@@ -19,7 +19,7 @@ const Hero = () => {
         <div className="flex justify-center relative my-20 z-index-11">
             <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center">
                 <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">Achraf Bayi&apos;s Portfolio</h2>
-                <TextGenerateEffect className="text-center text-[40px] md:text-5xl lg text-6xl" words="Welcome to my portfolio, where creative design meets thoughtful user experiences”/>
+                <TextGenerateEffect className="text-center text-[40px] md:text-5xl lg:text-6xl" words={"Welcome to my portfolio, where creative design meets thoughtful user experiences"}/>
                 <p className="text-center md:tracking-wider mb-4 text-sm md:text-large lg:text-2xl">Hi I&apos;m Achraf, an aspiring Software Engineer based in Canada.</p>
                 <a href="#about">
                     <MagicButton title="Show my work" icon={<FaLocationArrow/>} position='right'/>
