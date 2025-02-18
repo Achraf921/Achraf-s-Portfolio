6de046fa96b6c1adb8bff098d9176dcd9d9c934e
Achraf Bayi <achrafbayi@icloud.com>
2025-02-17 23:02:54 -0500

    portfolio

---
+++ components/Footer.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import MagicButton from './ui/MagicButton';
+import { FaLocationArrow } from 'react-icons/fa';
+import { socialMedia } from '@/data';
+
+const Footer = () => {
+  return (
+    <footer className="w-full pb-10 mb-[100px] sm:mb-5" id="contact">
+        <div className='flex flex-col items-center'>
+            <h1 className="heading lg:max-w-[45vw]">Looking for fresh talent? <span className="text-purple">Contact me</span>.</h1>
+            <p className="text-white-200 md:mt-10 my-6 text-center">Let&apos;s connect. <span className="text-purple">Contact me</span> today, and we'll explore how we can work together.</p>
+            <a href="mailto:achrafbayi@icloud.com">
+                <MagicButton title="Let's get in touch" icon={<FaLocationArrow/>} position="right"/>
+            </a>
+        </div>
+        <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+            <p className="md:text-base text-sm md:font-normal font-light">Copyright © 2025 Achraf</p>
+            <div className="flex items-center md:gap-3 gap-6">
+                {socialMedia.map((profile)=>(
+                    <div key={profile.id} className="w-10 name-10 cursor-pointer flex justify-center items-center backdrop-filter  backfrop-blue-lg saturate-180 bg-opacity-75 bg-black-200 border rounded-lg border-black-300 z-index-5">
+                        <img src={profile.img} alt={`${profile.id}`} width={20} height={20}/>
+                    </div>
+                ))}
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer;
---
+++ data/index.ts
@@ -0,0 +1,227 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "Pursuing a Bachelor of Engineering in Software Engineering at Concordia University ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "Can intern in Canada 🇨🇦 and the EU 🇪🇺",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently building a React Native mobile app",
+      description: "The Inside Scoop",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "EvoGym",
+      des: "EvoGym is a gym frontend project which demonstrates best practices in component-based development, showcasing a clean, responsive UI design.",
+      img: "/p1.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg"],
+      link: "https://evogymbyachraf.netlify.app/",
+    },
+    {
+      id: 2,
+      title: "MoneTrix - HackConcordiaIX",
+      des: "Collaborated on a full-stack financial app that uses receipt scanning to save money on purchases and groceries.",
+      img: "/p2.png",
+      iconLists: ["/re.svg", "/tail.svg", "/js.png", "/py.png", "pg.png"],
+      link: "https://monetrix.netlify.app/",
+    },
+    {
+      id: 3,
+      title: "NASA Astronomy Picture of the Day",
+      des: "Retrieved daily images from NASA's APOD API, showcasing dynamic content and scientific explanations.",
+      img: "/p67.png",
+      iconLists: ["/re.svg", "/tail.svg", "/js.png",],
+      link: "https://nasaapodbyachraf-c6vthfttw-achraf921s-projects.vercel.app/",
+    },
+    {
+      id: 4,
+      title: "LiftLogic - Workout Generator",
+      des: "Created a custom workout plan generator offering goal-based exercise routines and progress tracking.",
+      img: "/p4.png",
+      iconLists: ["/re.svg", "/tail.svg", "/js.png"],
+      link: "https://liftlogicbyachraf.netlify.app/#workout",
+    },
+    {
+      id: 5,
+      title: "CodeML, PolyAI Hackathon, ML seat selection predictor",
+      des: "Spearheaded the development a ML solution able to predict Air Canada’s user’s seat choice with a 92.07% accuracy.",
+      img: "/p5 2.png",
+      iconLists: ["/py.png","skl.png","pd.svg"],
+      link: "https://github.com/Achraf921/Hackathon-CodeML-2024",
+    },
+    {
+      id: 6,
+      title: "MyOnlinePokedex",
+      des: "MyOnlinePokedex is a React-based web application that serves as a comprehensive and interactive Pokédex.",
+      img: "/p6.png",
+      iconLists: ["/re.svg", "/tail.svg", "/js.png"],
+      link: "https://pokedexbyachraf.netlify.app/",
+    },
+    
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "",
+      name: "",
+      title: "",
+    },
+    {
+      quote:
+        "",
+      title: "",
+    },
+    {
+      quote:
+        "",
+      title: "",
+    },
+    {
+      quote:
+        "",
+      title: "",
+    },
+    {
+      quote:
+        "",
+      name: "",
+      title: "",
+    },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "",
+      img: "",
+      nameImg: "",
+    },
+    {
+      id: 2,
+      name: "",
+      img: "",
+      nameImg: "/",
+    },
+    {
+      id: 3,
+      name: "",
+      img: "",
+      nameImg: "",
+    },
+    {
+      id: 4,
+      name: "",
+      img: "",
+      nameImg: "",
+    },
+    {
+      id: 5,
+      name: "",
+      img: "",
+      nameImg: "",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "",
+      desc: "",
+      className: "",
+      thumbnail: "",
+    },
+    {
+      id: 2,
+      title: "",
+      desc: "",
+      className: "",
+      thumbnail: "",
+    },
+    {
+      id: 3,
+      title: "",
+      desc: "",
+      className: "",
+      thumbnail: "",
+    },
+    {
+      id: 4,
+      title: "",
+      desc: "",
+      className: "",
+      thumbnail: "",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+    },
+
+    {
+      id: 3,
+      img: "/link.svg",
+    },
+  ];
